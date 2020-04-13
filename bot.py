b0d20d3c35a5d557e533ccf9e325dc956857b5d0
ElFeeder <bernardo.galego@tecnico.ulisboa.pt>
2020-04-13 14:07:55 +0100

    Bot prevention

--- bot.py
+++ bot.py
@@ -41,6 +41,9 @@ def reply():
 
 
 def search():
+    # this is to prevent bot detection
+    phrase = 0
+
     # Retrieve the last seen tweet's ID
     lastNice = retrieve(niceFileName)
 
@@ -48,15 +51,32 @@ def search():
     numberNices = retrieve(niceCountFileName)
 
     nices = api.search(q = '#nice', since_id = lastNice)
+
+    # Reverse to see first the older tweets
     for nice in reversed(nices):
         print('Found #nice: ' + nice.text + '\n\tID: ' + str(nice.id)
             + '\nResponding...')
         lastNice = nice.id
         store(lastNice, niceFileName)
-        api.update_status('@' + nice.user.screen_name + ' Your nice hashtag was the '
-            + str(numberNices) + 'th since 13/04/2020 13:08! Nice!', nice.id)
+
+        if(phrase == 0):
+            api.update_status('@' + nice.user.screen_name +
+            ' Your nice hashtag was the ' + str(numberNices) +
+            'th since 13/04/2020! Nice!', nice.id)
+        elif(phrase == 1):
+            api.update_status('@' + nice.user.screen_name +
+            ' Since 13/04/2020, there has been ' + str(numberNices - 1) +
+            '. Yours was ' + str(numberNices) + '. Nice!', nice.id)
+        elif(phrase == 2):
+            api.update_status('@' + nice.user.screen_name +
+            ' With your nice hashtag, the number of nices said ince 13/04/2020 has inscreased to ' +
+            str(numberNices) + '. Nice!', nice.id)
 
         numberNices = numberNices + 1
+        phrase = phrase + 1
+        if(phrase == 3):
+            phrase = 0
+
         print('\n')
     store(numberNices, niceCountFileName)
 
@@ -68,18 +88,19 @@ niceCountFileName = 'NiceCount.txt'
 # You can find these keys in twitter developer
 consumerKey = 'ux2xvOZ7cBc1vIGZSthjhw0KW'
 consumerSecret = 'wZhFMH4zn1OjjERpkS1DW3FTGrf6ijWgIwBylP0p8oNHZYwP8K'
-accessKey = '1249442524873146369-YGJLKzvqW4sNFpBcd8q87Du3kqa4V8'
-accessSecret = '7jschEQbz3TSp4wlm2RkFVivwjWllWPQWiwSJJ9rPZpSj'
+accessKey = '1249442524873146369-Zfpo4dTW6qr0o5H3wROuFywhtfZeSF'
+accessSecret = 'YkdQU8bGgP2DXLYU0A2FJToIiVvaO5qb3Z55NSVabdp1a'
 
 auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
 auth.set_access_token(accessKey, accessSecret)
 api = tweepy.API(auth)
 
-# Infinite loop, always responding (20 in 20 seconds)
+# Infinite loop, always responding (40 in 40 seconds)
 while True:
     #print('Checking for mentions...')
     #reply()
 
     print('Checking for #nice...')
     search()
-    time.sleep(20)
+    print('Waiting 40 seconds')
+    time.sleep(40)
