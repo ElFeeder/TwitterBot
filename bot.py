ba4d718eb7b0158654320eb71de5c2ef5db06cbb
ElFeeder <bernardo.galego@tecnico.ulisboa.pt>
2020-04-13 13:30:36 +0100

    It works

--- bot.py
+++ bot.py
@@ -2,23 +2,24 @@ import tweepy
 import time
 
 
-def retrieveID(fileName):
+
+def retrieve(fileName):
     textFile = open(fileName, 'r')
-    lastID = int(textFile.read().strip())
+    ID = int(textFile.read().strip())
     textFile.close()
-    return lastID
+    return ID
 
 
-def storeID(lastID, fileName):
+def store(ID, fileName):
     textFile = open(fileName, 'w')
-    textFile.write(str(lastID))
+    textFile.write(str(ID))
     textFile.close()
     return
 
 
 def reply():
     # Retrieve the last seen tweet's ID
-    lastID = retrieveID(fileName)
+    lastID = retrieve(mentionsFileName)
 
     #Uncomment next line for testing purposes
     #lastID = 1249479111891914752
@@ -30,7 +31,7 @@ def reply():
     for mention in reversed(mentions):
         print('Found mention: ' +  mention.text + '\n\tID: ' + str(mention.id))
         lastID = mention.id
-        storeID(lastID, fileName)
+        store(lastID, mentionsFileName)
         if('drena' in mention.text.lower()):
             print('found drena, responding')
             api.update_status('@' + mention.user.screen_name + ' That\'s the spirit!',
@@ -39,7 +40,30 @@ def reply():
         print('\n')
 
 
-fileName = 'LastID.txt'
+def search():
+    # Retrieve the last seen tweet's ID
+    lastNice = retrieve(niceFileName)
+
+    # Retrieve number of nices to this point
+    numberNices = retrieve(niceCountFileName)
+
+    nices = api.search(q = '#nice', since_id = lastNice)
+    for nice in reversed(nices):
+        print('Found #nice: ' + nice.text + '\n\tID: ' + str(nice.id)
+            + '\nResponding...')
+        lastNice = nice.id
+        store(lastNice, niceFileName)
+        api.update_status('@' + nice.user.screen_name + ' Your nice hashtag was the '
+            + str(numberNices) + 'th since 13/04/2020 13:08! Nice!', nice.id)
+
+        numberNices = numberNices + 1
+        print('\n')
+    store(numberNices, niceCountFileName)
+
+
+mentionsFileName = 'MentionsID.txt'
+niceFileName = 'NiceID.txt'
+niceCountFileName = 'NiceCount.txt'
 
 # You can find these keys in twitter developer
 consumerKey = 'ux2xvOZ7cBc1vIGZSthjhw0KW'
@@ -53,6 +77,9 @@ api = tweepy.API(auth)
 
 # Infinite loop, always responding (20 in 20 seconds)
 while True:
-    print('Checking for mentions...')
-    reply()
+    #print('Checking for mentions...')
+    #reply()
+
+    print('Checking for #nice...')
+    search()
     time.sleep(20)
