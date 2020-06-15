4407d46e2cbd48d96705ce3ad80ce113ba99e08a
ElFeeder <bernardo.galego@tecnico.ulisboa.pt>
2020-06-15 12:40:25 +0100

    Error handling

--- Bot.py
+++ Bot.py
@@ -28,35 +28,39 @@ def search():
 
     nices = api.search(q = '#nice', since_id = lastNice)
 
-    # Reverse to see first the older tweets
-    for nice in reversed(nices):
-        print('Found #nice: ' + nice.text + '\n\tID: ' + str(nice.id)
-            + '\n\tUsername: ' + nice.user.screen_name + '\nResponding...')
-        lastNice = nice.id
-        store(lastNice, niceFileName)
+    # We'll try to find a tweet; if error just wait and repeat
+    try:
+        # Reverse to see first the older tweets
+        for nice in reversed(nices):
+            print('Found #nice: ' + nice.text + '\n\tID: ' + str(nice.id)
+                + '\n\tUsername: ' + nice.user.screen_name + '\nResponding...')
+            lastNice = nice.id
+            store(lastNice, niceFileName)
 
-        # This is here because of bot prevention
-        numberNices = numberNices + 1
-        store(numberNices, niceCountFileName)
-		
-        if(phrase == 0):
-            api.update_status('@' + nice.user.screen_name +
-            ' Your nice hashtag was the ' + str(numberNices) +
-            'th since 13/04/2020 13:08! Nice!', nice.id)
-        elif(phrase == 1):
-            api.update_status('@' + nice.user.screen_name +
-            ' Since 13/04/2020 13:08, there has been ' + str(numberNices - 1) +
-            ' nice hashtags. Yours was ' + str(numberNices) + '. Nice!', nice.id)
-        elif(phrase == 2):
-            api.update_status('@' + nice.user.screen_name +
-            ' With your nice hashtag, the number of nices said since 13/04/2020 13:08 has increased to ' +
-            str(numberNices) + '. Nice!', nice.id)
+            # This is here because of bot prevention
+            numberNices = numberNices + 1
+            store(numberNices, niceCountFileName)
+            
+            if(phrase == 0):
+                api.update_status('@' + nice.user.screen_name +
+                ' Your nice hashtag was the ' + str(numberNices) +
+                'th since 13/04/2020 13:08! Nice!', nice.id)
+            elif(phrase == 1):
+                api.update_status('@' + nice.user.screen_name +
+                ' Since 13/04/2020 13:08, there has been ' + str(numberNices - 1) +
+                ' nice hashtags. Yours was ' + str(numberNices) + '. Nice!', nice.id)
+            elif(phrase == 2):
+                api.update_status('@' + nice.user.screen_name +
+                ' With your nice hashtag, the number of nices said since 13/04/2020 13:08 has increased to ' +
+                str(numberNices) + '. Nice!', nice.id)
 
-        phrase = phrase + 1
-        if(phrase == 3):
-			phrase = 0
+            phrase = phrase + 1
+            if(phrase == 3):
+                phrase = 0
 
-        print('\n')
+            print('\n')
+    except:
+        print("Error found, waiting 60 seconds and retrying")
 
 
 mentionsFileName = 'MentionsID.txt'
