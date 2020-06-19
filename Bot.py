e603fe5eb3be641e829a83eecce9f4482eaa2fb6
ElFeeder <bernardo.galego@tecnico.ulisboa.pt>
2020-06-19 14:06:03 +0100

    New keys, wtf is this programming bruh?

--- Bot.py
+++ Bot.py
@@ -28,39 +28,36 @@ def search():
 
     nices = api.search(q = '#nice', since_id = lastNice)
 
-    # We'll try to find a tweet; if error just wait and repeat
-    try:
-        # Reverse to see first the older tweets
-        for nice in reversed(nices):
-            print('Found #nice: ' + nice.text + '\n\tID: ' + str(nice.id)
-                + '\n\tUsername: ' + nice.user.screen_name + '\nResponding...')
-            lastNice = nice.id
-            store(lastNice, niceFileName)
 
-            # This is here because of bot prevention
-            numberNices = numberNices + 1
-            store(numberNices, niceCountFileName)
+    # Reverse to see first the older tweets
+    for nice in reversed(nices):
+        print('Found #nice: ' + nice.text + '\n\tID: ' + str(nice.id)
+              + '\n\tUsername: ' + nice.user.screen_name + '\nResponding...')
+        lastNice = nice.id
+        store(lastNice, niceFileName)
+
+        # This is here because of bot prevention
+        numberNices = numberNices + 1
+        store(numberNices, niceCountFileName)
             
-            if(phrase == 0):
-                api.update_status('@' + nice.user.screen_name +
-                ' Your nice hashtag was the ' + str(numberNices) +
-                'th since 13/04/2020 13:08! Nice!', nice.id)
-            elif(phrase == 1):
-                api.update_status('@' + nice.user.screen_name +
-                ' Since 13/04/2020 13:08, there has been ' + str(numberNices - 1) +
-                ' nice hashtags. Yours was ' + str(numberNices) + '. Nice!', nice.id)
-            elif(phrase == 2):
-                api.update_status('@' + nice.user.screen_name +
-                ' With your nice hashtag, the number of nices said since 13/04/2020 13:08 has increased to ' +
-                str(numberNices) + '. Nice!', nice.id)
+        if(phrase == 0):
+            api.update_status('@' + nice.user.screen_name +
+            ' Your nice hashtag was the ' + str(numberNices) +
+            'th since 13/04/2020 13:08! Nice!', nice.id)
+        elif(phrase == 1):
+            api.update_status('@' + nice.user.screen_name +
+            ' Since 13/04/2020 13:08, there has been ' + str(numberNices - 1) +
+            ' nice hashtags. Yours was ' + str(numberNices) + '. Nice!', nice.id)
+        elif(phrase == 2):
+            api.update_status('@' + nice.user.screen_name +
+            ' With your nice hashtag, the number of nices said since 13/04/2020 13:08 has increased to ' +
+            str(numberNices) + '. Nice!', nice.id)
 
-            phrase = phrase + 1
-            if(phrase == 3):
-                phrase = 0
+        phrase = phrase + 1
+        if(phrase == 3):
+            phrase = 0
 
-            print('\n')
-    except:
-        print("Error found, waiting 60 seconds and retrying")
+        print('\n')
 
 
 mentionsFileName = 'MentionsID.txt'
@@ -68,10 +65,10 @@ niceFileName = 'NiceID.txt'
 niceCountFileName = 'NiceCount.txt'
 
 # You can find these keys in twitter developer
-consumerKey = 'ux2xvOZ7cBc1vIGZSthjhw0KW'
-consumerSecret = 'wZhFMH4zn1OjjERpkS1DW3FTGrf6ijWgIwBylP0p8oNHZYwP8K'
-accessKey = '1249442524873146369-Zfpo4dTW6qr0o5H3wROuFywhtfZeSF'
-accessSecret = 'YkdQU8bGgP2DXLYU0A2FJToIiVvaO5qb3Z55NSVabdp1a'
+consumerKey = 'lXp04hHaxBiWFcV4D3HHDV2kb'
+consumerSecret = '0z68Oa4mZolfir68NPaPck9FFtFl3Pe0wykQ24H74YzN3jLhKQ'
+accessKey = '1249442524873146369-J7VLbZeUMLaMGbUgObL4MYi9gL6Zps'
+accessSecret = 'dxnw9vpDG8nzIhZVOCOxgUdyyXXOFZy2BfoADTWegZcJg'
 
 auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
 auth.set_access_token(accessKey, accessSecret)
